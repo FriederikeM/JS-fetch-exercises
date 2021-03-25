c45a3d3706cfb79df5ebfe086fc3f5a0f4a06cde
Friederike Moeser <friederike.moeser@gmail.com>
2021-03-25 15:05:12 +0100

    Solve exercise 3

--- 3-catching-errors/solution.js
+++ 3-catching-errors/solution.js
@@ -2,6 +2,8 @@ const url = "https://.typicode.com/todos/x";
 
 fetch(url)
   .then((response) => {
+    // won't be excuted, because error happens before
+    console.log(response);
     if (response.ok) {
       return response.json();
     } else {
